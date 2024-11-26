ecc25512a7b2c847ea45210cc192e36aa9cf4e99
Marcos-Monte <marcos.monte.junior@gmail.com>
2024-11-26 17:05:37 -0300

    feat: contruido os scores dos personagens

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ new Vue({
     el: '#desafio',
 
     data: {
-        
+        lifeJogador: 74,
+        lifeMonstro: 24,
+        jogando: false
     },
 
     computed: {
+        temResultado(){
+            this.jogando = false;
 
+        }
     },
     methods: {
 
     },
     watch: {
-        
+
     }
 })
