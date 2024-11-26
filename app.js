17f9370e6fab78aee7510d7945f79c6752b1aa7e
Marcos-Monte <marcos.monte.junior@gmail.com>
2024-11-26 15:59:18 -0300

    restart do projeto

--- app.js
+++ app.js
@@ -1,115 +1,18 @@
 new Vue({
+
     el: '#desafio',
-    data :{
-        lifeJogador: 100,
-        lifeMonstro: 100,
-        golpeEspecial: 5,
-        iniciado: false,
-        log: []
+
+    data: {
+        
+    },
+
+    computed: {
+
     },
     methods: {
-        lifeColor(life){
-            if(life >= 75){
-                return 'saudavel';
-            } else if (life < 75 && life >= 25){
-                return 'ferido';
-            } else {
-                return 'perigo';
-            }
-        },
+
+    },
+    watch: {
         
-        curar(cura){
-            this.lifeJogador = Math.min(this.lifeJogador + cura, 100)
-            
-        },
-
-        ataqueMonstro(dano){
-            this.lifeJogador = Math.max(this.lifeJogador - dano, 0)
-        },
-
-        ataqueJogador(dano){
-            this.lifeMonstro = Math.max(this.lifeMonstro - dano, 0)
-        },
-        
-        listarAtaques(jogador, monstro){
-
-            // Adiciona o dano do jogador ao log
-            this.log.push(`Dano Jogador: ${jogador}`);
-            // Adiciona o dano do monstro ao log
-            this.log.push(`Dano Monstro: ${monstro}`);
-
-            /* console.log(this.log) */
-        },
-
-        listarCura(cura, monstro){
-
-            // Adiciona o dano do jogador ao log
-            this.log.push(`Curando Jogador: ${cura}`);
-            // Adiciona o dano do monstro ao log
-            this.log.push(`Dano Monstro: ${monstro}`);
-
-            /* console.log(this.log) */
-        },
-
-        listarEspecial(especial, monstro){
-
-            // Adiciona o dano do jogador ao log
-            this.log.push(`Especial Jogador: ${especial}`);
-            // Adiciona o dano do monstro ao log
-            this.log.push(`Dano Monstro: ${monstro}`);
-
-            this.golpeEspecial -= 1;
-
-            /* console.log(this.log) */
-        },
-
-        acao(acao){
-
-            let danoJogador = 0;
-            let danoMonstro = 0;
-            let cura = 0;
-            let especial = 0;
-
-            if(acao === 'atacar'){
-               danoMonstro = Number((Math.random() * 5)).toFixed(0)
-               danoJogador = Number((Math.random() * 5)).toFixed(0);
-    
-                this.ataqueJogador(danoJogador)
-                this.ataqueMonstro(danoMonstro);
-                
-                this.listarAtaques(danoJogador, danoMonstro)
-
-            } else if (acao === 'curar'){
-                cura = Number((Math.random() * 50)).toFixed(0);
-                danoMonstro = Number((Math.random() * 5)).toFixed(0)
-    
-                this.curar(cura)
-                this.ataqueMonstro(danoMonstro);
-                
-                this.listarCura(cura, danoMonstro)
-
-            } else if(acao === 'especial'){
-                danoMonstro = Number((Math.random() * 5)).toFixed(0)
-                especial = Number((Math.random() * 5)).toFixed(0) * 2;
-
-                this.ataqueJogador(especial)
-                this.ataqueMonstro(danoMonstro);
-                
-                this.listarEspecial(especial, danoMonstro)
-
-            }
-            
-            // Log no console para debug
-            console.log(`Jogador: Dano ${danoJogador}, Cura ${cura}, Especial ${especial} | Monstro: Dano ${danoMonstro}`);
-        },
-
-        reset(){
-            this.lifeJogador = 100,
-            this.lifeMonstro = 100,
-            this.golpeEspecial= 5,
-            this.iniciado = false,
-            this.log = []
-        }
-
     }
 })
