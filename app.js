12a008e38bbc518d2de9435654291c1d3c5e511c
Marcos-Monte <marcos.monte.junior@gmail.com>
2024-11-27 11:27:48 -0300

    docs: Adicionando comentários na camada de serviços e na instancia Vue

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+/* Importando funções: Estabelecer dano e cura, da camada de serviços */
 import { curar, dano } from './services/services.js';
 
+/* Instancia Vue, Responsável pela interação com o template html */
 new Vue({
-
+    /* Qual elemento deve ser monitorado */
     el: '#desafio',
 
+    /* Dados / Variáveis */
     data: {
         lifeJogador: 100,
         lifeMonstro: 100,
@@ -11,13 +14,17 @@ new Vue({
         logs: []
     },
 
+    /* Métodos Computados - São executados, apenas, se as dependencias mudarem */
     computed: {
+        /* Retorna valor 'true' se uma das condições forem alcançadas */
         temResultado(){
             return this.lifeJogador == 0 || this.lifeMonstro == 0
         }
     },
 
+    /* Métodos - Executam lógica ou ação */
     methods: {
+        /* Reseta valores dos dados / variáveis. Inicia o Jogo */
         iniciarPartida(){
             this.jogando = true;
             this.lifeJogador = 100;
@@ -25,29 +32,41 @@ new Vue({
             this.logs = [];
         },
 
+        /* Função de ataque */
         atacar(especial){
 
+            /* Logica de recebimento de dano do personagem Monstro */
+            /* Pode ou não receber dano 'especial' */
             dano(this, 'lifeMonstro', 5, 10, especial, 'jogador', 'monstro', 'player', 'logs')
 
+            /* Condicional: Se o life do Monstro for maior que zero, pode atacar. */
             if(this.lifeMonstro > 0){
+                /* Logica de recebimento de dano do personagem Monstro  */
                 dano(this, 'lifeJogador', 7, 12, false, 'monstro', 'jogador', 'monster', 'logs')
             }
             
 
         },
 
+        /* Função de curar em Batalha */
         curarEmBatalha(){
 
+            /* Logica de recebimento de dano do personagem Monstro  */
             dano(this, 'lifeJogador', 7, 12, false, 'monstro', 'jogador', 'monster', 'logs')
 
+            /* Logica de recebimento de cura do personagem Jogador  */
             curar(this, 'lifeJogador', 8, 13, 'player', 'logs')
 
         },
         
     },
-    
+
+    /* Metodo Watch - Observa mudanças em propriedades ou métodos. Usado para realizar ações assincronas ou reações a mudanças de dados. Não retorna um valor */
     watch: {
 
+        /* Se o Método Computado receber o valor, a propriedade 'jogando' recebe o valor 'falso'.
+        
+        A lógica refere a: Se tem um resultado, então não estão mais jogando*/
         temResultado(valor){
 
             if(valor) this.jogando = false;
--- services/services.js
+++ services/services.js
@@ -1,13 +1,21 @@
+/* Função que gera um valor aleatorio - recebe um 'range' maximo e minimo */
 export function valorAleatorio(min, max){
 
+    /* Lógica que gera um valor aleatorio entre: menor valor e maior valor */
     const valor = Math.random() * (max - min) + min;
 
+    /* Arredonda o valor para cima ou para baixo */
     return Math.round(valor);
 
 }
 
+/* Função de registro de acontecimento em uma lista (Logs) 
+    - recebe a classe de estilização que aquele registro deve receber
+    - a lista onde deve ser armazenada
+    - texto exibido*/
 export function registrarLog(classe, lista, texto){
 
+    /* Puxa o registro para a lista - Adicionando o novo em cima e não em baixo da pilha */
     lista.unshift({
 
         texto, classe
@@ -16,21 +24,43 @@ export function registrarLog(classe, lista, texto){
 
 }
 
+/* Função que aplica e registra dano, recebe:
+    - contexto = em qual contexto essa função vai rodar, nesse caso 'this'
+    - alvo = qual variavel vai sofrer alteração (lifeJogador ou lifeMonstro)
+    - min e max = estabelece o 'range' do dano.
+    - especial = recebe fazer falso ou true. 
+    - atacante, vitima, classe e lista = propriedades passadas para a função de 'registro de log'
+     */
 export function dano(contexto, alvo, min, max, especial, atacante, vitima, classe, lista){
+
+    /* Se o golpe especial, a variavel vai receber +5 */
     const plus = especial? 5 : 0;
 
+    /* variavel dano recebe valor aleatorio. Range recebe +5 nos valores minimo e maximo */
     const dano = valorAleatorio(min + plus, max + plus)
 
-    contexto[alvo] = Math.max(contexto[alvo]  - dano, 0);
+    /* Alvo recebe o valor de dano. Escolhe o valor 'maior' entre a 'logica' e zero
+    - Impede o 'life' ficar negativo */
+    contexto[alvo] = Math.max(contexto[alvo] - dano, 0);
 
+    /* Registrando 'dano' na lista e com o texto indicado */
     registrarLog(classe, contexto[lista], `${atacante} atingiu ${vitima} com ${dano}.`)
 }
 
+/* Função que aplica cura, recebe: 
+    - contexto = em qual contexto essa função vai rodar, nesse caso 'this'
+    - alvo = qual variavel vai sofrer alteração (lifeJogador ou lifeMonstro)
+    - min e max = estabelece o 'range' da cura.
+    - classe e lista = propriedades passadas para a função de 'registro de log'*/
 export function curar(contexto, alvo, min, max, classe, lista){
 
+    /*Variavel de cura, recebe valor aleatorio dentro do seu range */
     const cura = valorAleatorio(min, max);
 
+    /* Alvo recebe o valor de cura. Escolhe o valor 'menor' entre a 'logica' e 100
+    - Impede o 'life' ficar maior que 100 */
     contexto[alvo] = Math.min(contexto[alvo] + cura, 100);
 
+     /* Registrando 'cura' na lista e com o texto indicado */
     registrarLog(classe, contexto[lista], `jogador foi curado em ${cura}.`)
 }
